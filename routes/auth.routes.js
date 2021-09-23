8e8c0f4c47cf73fb6ddfd55c99b395ed1777c456
Alexander Jinoridze <sanjinoridze@gmail.com>
2021-09-24 03:11:22 +0400

    Add user registration logic

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,24 @@
 const { Router } = reuqire("express");
+const bcrypt = reuqire("bcryptjs");
 const User = require("../models/User");
 const router = Router();
 
 // /api/auth/register
 router.post("/register", async (req, res) => {
     try {
-
         const { email, password } = req.body;
+        const candidate = await User.findOne({ email });
 
+        if(candidate) {
+            return res.status(400).json({ message: "User with this Email already exists" });
+        }
 
+        const hashedPassword = bcrypt.hash(password, 12);
+        const user = new User({ email, password: hashedPassword });
+
+        await user.save();
+
+        res.status(201).json({ message: "User created" });
 
     } catch(e) {
         res.status(500).json({ message: "Something went wrong, try next time" });
