ee2971688c7c6bc4b35bf1934ba09983fceb29f9
Alexander Jinoridze <sanjinoridze@gmail.com>
2021-09-24 16:57:33 +0400

    Add refferenc to Results schema in User schema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const schema = new Schema({
     username: { type: String, require: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    links: [{ type: Types.ObjectId, ref: "Link" }]
+    results: [{ type: Types.ObjectId, ref: "Results" }]
 });
 
 module.exports = model("User", schema);
