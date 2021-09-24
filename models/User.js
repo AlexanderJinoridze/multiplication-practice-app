1361a37735cf063e0859af0f2acee699df6b7c35
Alexander Jinoridze <sanjinoridze@gmail.com>
2021-09-24 16:54:14 +0400

    Add username in User schema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model, Types } = require("mongose");
 
 const schema = new Schema({
+    username: { type: String, require: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     links: [{ type: Types.ObjectId, ref: "Link" }]
