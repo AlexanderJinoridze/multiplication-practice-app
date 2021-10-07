9b686bc90409473934683dbe593085ae1b347d2f
Alexander Jinoridze <sanjinoridze@gmail.com>
2021-10-07 04:36:46 +0400

    Add dashboard page

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,23 +1,37 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Loader from "../components/Loader";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { AuthContext } from "../context/AuthContext";
 import useHttp from "../hooks/http.hook";
+import toast from 'react-hot-toast';
 
 
 export default function DashboardPage() {
-    const [username, setUsername] = useState("");
+    const [userData, setUserData] = useState({usename: "", email: ""});
 
     const { loading, request } = useHttp();
-    const { token, userId } = useContext(AuthContext);
+    const { token, userId, logout } = useContext(AuthContext);
 
     const fetchUser = useCallback(async () => {
         try {
             const fetched = await request(`/api/user/${ userId }`, "GET", null, {
                 Authorization: `Bearer ${ token }`
             });
-            setUsername(fetched.username);
+            setUserData({ username: fetched.username, email: fetched.email });
         } catch(e) {}
     }, [token, userId, request]);
+
+    const deleteUser = useCallback(async () => {
+        try {
+            const data = await request(`/api/user/delete/${ userId }`, "DELETE", null, {
+                Authorization: `Bearer ${ token }`
+            });
+            logout();
+            toast.success(data.message);
+        } catch(e) {}
+    }, [token, userId, request, logout]);
 
     useEffect(() => {
         fetchUser();
@@ -29,10 +43,26 @@ export default function DashboardPage() {
 
     return(
         <>
-        {!loading && <div>
-            <h1>Dashboard</h1>
-            <span>{ username }</span>
-        </div>}
+        {!loading
+        &&
+        <div id="page">
+            <Navbar />
+            <main className="container flex flex-col justify-center items-center text-center max-w-4xl">
+                <h2 className="mb-2">Welcome <b>{ userData.username }</b></h2>
+                <h5>Your email is <i>{ userData.email }</i></h5>
+                <div className="flex mt-8 justify-center">
+                    <Link className="btn btn-primary mx-2" to="/practice">Practice</Link>
+                    <Link className="btn btn-primary mx-2" to="/tips">Tips & Tricks</Link>
+                    <Link className="btn btn-primary mx-2" to="/statistics">Statistics</Link>
+                </div>
+                <div className="flex flex-col items-center mt-32">
+                    <span>Want to permanemtly delete your account and progress?</span>
+                    <button onClick={ deleteUser } className="btn btn-danger mt-4 flex flex-row items-center"><i className="icon-trash mr-2"></i>Delete account<div className="loader"></div></button>
+                </div>
+            </main>
+            <Footer />
+        </div>
+        }
         </>
     );
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,4 +12,13 @@ router.get("/:id", auth, async (req, res) => {
     }
 });
 
+router.delete("/delete/:id", auth, async (req, res) => {
+    try {
+        await User.findByIdAndRemove(req.params.id);
+        res.json({ message: "User deleted"});
+    } catch(e) {
+        res.status(500).json({ message: "Something went wrong"});
+    }
+});
+
 module.exports = router;
